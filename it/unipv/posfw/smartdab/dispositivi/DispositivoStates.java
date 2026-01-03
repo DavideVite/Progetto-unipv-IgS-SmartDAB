876cffa07aaad49f1c5fb3dec069d6669283ebcf
AlezGradient <AlessandroGere.Ingenito01@universitadipavia.it>
2026-01-03 15:02:36 +0100

    Feature/dispositivo (#7)
    * Risoluzione problemi dispositivo ed interfacce Observable e Observer

--- it/unipv/posfw/smartdab/dispositivi/DispositivoStates.java
+++ it/unipv/posfw/smartdab/dispositivi/DispositivoStates.java
@@ -1,5 +1,5 @@
 package it.unipv.posfw.smartdab.dispositivi;
 
 public enum DispositivoStates {
-	ALIVE, CONFLICT, UNKNOWN
+	ALIVE, CONFLICT, UNKNOWN, DISABLED
 }
--- it/unipv/posfw/smartdab/interfaces/Observer.java
+++ it/unipv/posfw/smartdab/interfaces/Observer.java
@@ -1,7 +1,5 @@
 package it.unipv.posfw.smartdab.interfaces;
 
-import java.util.EventObject;
-
 public interface Observer {
 	public void update(Observable o, Object args);
 }
