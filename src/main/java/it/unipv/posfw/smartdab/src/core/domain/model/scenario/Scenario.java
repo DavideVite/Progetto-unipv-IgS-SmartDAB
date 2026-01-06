6a8c89e84ddd877a4c6c72113a62e2c129d76269
DavideVite <davide.vitello01@universitadipavia.it>
2026-01-06 10:23:36 +0100

    create EnumScenarioType e Scenario

---
+++ src/main/java/it/unipv/posfw/smartdab/src/core/domain/model/scenario/Scenario.java
@@ -0,0 +1,106 @@
+package main.java.it.unipv.posfw.smartdab.src.core.domain.model.scenario;
+import main.java.it.unipv.posfw.smartdab.src.core.domain.enums.EnumScenarioType; 
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Scenario {
+
+//	LocalDateTime() è un classe che rappresenta data e ora senza fuso orario
+//	- non tiene traccia del timezone 
+//	- formato : "2025-01-05T14:30:00"
+//	E' uno standard per gestire il tempo e permette vari modi di creazione e operazioni comuni. 
+	
+	
+	private String nome; 
+	private List<ScenarioStanzaConfig> configurazioni; 
+	private EnumScenarioType tipo_scenario; 
+	private boolean isActive; 
+	private LocalDateTime data_creazione; 
+	private	LocalDateTime data_ultima_modifica; 
+	
+	
+	public Scenario(String nome) {
+		this.nome = nome; 
+		this.configurazioni = new ArrayList<>();
+		this.tipo_scenario = EnumScenarioType.PERSONALIZZATO; 
+		this.isActive = false; 
+		this.data_creazione = LocalDateTime.now();
+		this.data_ultima_modifica = LocalDateTime.now();
+	}
+	
+	public Scenario(String nome, EnumScenarioType tipo_scenario) {
+		this.nome = nome; 
+		this.configurazioni = new ArrayList<>();
+		this.tipo_scenario = tipo_scenario; 
+		this.isActive = false; 
+		this.data_creazione = LocalDateTime.now();
+		this.data_ultima_modifica = LocalDateTime.now();
+		
+	}
+	
+	public void aggiungiConfigurazione(ScenarioStanzaConfig config) {
+	       configurazioni.add(config);
+	       this.data_ultima_modifica = LocalDateTime.now();
+	
+	 }
+	 
+	 public void rimuoviConfigurazione(ScenarioStanzaConfig config) {
+		   configurazioni.remove(config); 
+		   this.data_ultima_modifica = LocalDateTime.now(); 
+		 
+	 }
+	 
+
+	 // Attiva e Disattiva Scenario 
+	 public void attivaScenario() {
+		 this.isActive = true; 
+	 }
+	 
+	 public void disattivaScenario() {
+		 this.isActive = false; 
+		 
+	 }
+	 
+	 
+	 // Getters 
+	 public String getNome() {
+		 return nome;  
+	 }
+	 
+	 public List<ScenarioStanzaConfig> getConfigurazioni() {
+	        return configurazioni;
+	    }
+	 
+	 public EnumScenarioType getTipo_scenario() {
+		return tipo_scenario;
+	}
+
+	 public boolean isActive() {
+		 return isActive;
+	 }
+
+	 public LocalDateTime getData_creazione() {
+		return data_creazione;
+	}
+
+
+	 public LocalDateTime getData_ultima_modifica() {
+		 return data_ultima_modifica;
+	 }
+
+
+	 
+	 // to String
+	 
+	 @Override
+	 public String toString() {
+		return "Scenario [nome=" + nome + ", " + "\nconfigurazioni=" + configurazioni + "]";
+	 }
+	 
+	 
+	 
+	 
+}
