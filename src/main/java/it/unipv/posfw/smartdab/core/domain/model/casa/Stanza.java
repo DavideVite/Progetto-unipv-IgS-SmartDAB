c18e22e2b6f36d532a4fcb1d1d4f62eca50a995f
DavideVite <davide.vitello01@universitadipavia.it>
2026-01-09 16:12:54 +0100

    Aggiornato Stanza, Casa e GestoreStanze

--- src/main/java/it/unipv/posfw/smartdab/core/domain/model/casa/Stanza.java
+++ src/main/java/it/unipv/posfw/smartdab/core/domain/model/casa/Stanza.java
@@ -1,14 +1,16 @@
 package it.unipv.posfw.smartdab.core.domain.model.casa;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import it.unipv.posfw.smartdab.core.domain.model.dispositivo.Dispositivo;
-import it.unipv.posfw.smartdab.core.domain.model.parametro.ObservableParameter;
-import it.unipv.posfw.smartdab.core.port.communication.observer.Observable;
-import it.unipv.posfw.smartdab.core.port.communication.observer.Observer;
+import src.main.java.it.unipv.posfw.smartdab.src.adapter.facade.SensoreFacade;
+import src.main.java.it.unipv.posfw.smartdab.src.core.domain.model.dispositivo.Dispositivo;
+import src.main.java.it.unipv.posfw.smartdab.src.core.domain.model.parametro.ObservableParameter;
+import src.main.java.it.unipv.posfw.smartdab.src.core.port.communication.observer.Observable;
+import src.main.java.it.unipv.posfw.smartdab.src.core.port.communication.observer.Observer;
 
 public class Stanza implements Observable, Observer{
 	 private String id;
@@ -16,12 +18,12 @@ public class Stanza implements Observable, Observer{
 	 private List<Dispositivo> dispositivi = new ArrayList<>();
 	 private Map<String, Double> parametri = new HashMap<>();
 	 private List<Observer> observers = new ArrayList<>(); 
-	 
+
 	 public Stanza(String id, String nome) {
 		 this.id = id;
 		 this.nome = nome;	 
 	 }
-	 
+
 	 public String getId() {
 		 return id;
 	 }
@@ -37,41 +39,37 @@ public class Stanza implements Observable, Observer{
 	 public List<Dispositivo> getDispositivi() {
 		 return dispositivi;
 	 }
-	 
+
 	 public Map<String, Double> getParametri() {
 		 return parametri;
 	 }
 
-	 public Double getMisura(String p) {
-		 return parametri.get(p);
+	 public Double getMisura(String parametro) {
+		 return parametri.get(parametro);
 	 }
-	 
+
 	 public boolean isEmpty() {
 		 if (dispositivi == null || dispositivi.isEmpty()) {
 				 return true;  
 	     }
 	     return false;
 	 }
-	 
+
 	 public void addDispositivo(Dispositivo d) {
 		 if (d != null) {
 			 this.dispositivi.add(d);
-			 
-			 notifyObservers("dispositivo_aggiunto");
 		 }
 	 }
-	 
+
 	 public void removeDispositivo(Dispositivo d) {
 		 if (d != null) {
 			 this.dispositivi.remove(d);
-			 
-			 notifyObservers("dispositivo_rimosso");
 		 }
 	 }
-	 
+
 	 public void updateParameter(String nomeParametro, double nuovoValore) {
 		 this.parametri.put(nomeParametro, nuovoValore);
-		 
+
 		 notifyObservers(nomeParametro);
 	 }
 
@@ -84,25 +82,26 @@ public class Stanza implements Observable, Observer{
 	 public void removeObserver(Observer observer) {
     	 observers.remove(observer);	     
      }
-	 
+
      @Override
 	 public void notifyObservers(Object args) {
-         for(Observer observer : observers) {
-        	 observer.update(this, args);
+         for (Observer o : observers) {
+        	 if (o instanceof SensoreFacade) {
+        		 SensoreFacade s = (SensoreFacade) o;
+        		 s.update(this, args);
+        	 }
          }
          }
-     
+
      @Override
      public void update(Observable o, Object arg) {
          ObservableParameter obsParam = (ObservableParameter) o;
-        	
+
          String nome = obsParam.getParameterName();
          double valore = obsParam.getValue();
-    	 
+
          this.updateParameter(nome, valore);
          }
      }
- 
-
 
 
