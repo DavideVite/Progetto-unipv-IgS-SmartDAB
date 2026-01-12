976533e10061f8838e9fabb8432badad5e2986e9
AlezGradient <AlessandroGere.ingenito01@universitadipavia.it>
2026-01-12 10:25:57 +0100

    Aggiunta enum parameters

--- src/main/java/it/unipv/posfw/smartdab/core/domain/model/dispositivo/attuatori/lampadaON_OFF/Lampada_ON_OFF.java
+++ src/main/java/it/unipv/posfw/smartdab/core/domain/model/dispositivo/attuatori/lampadaON_OFF/Lampada_ON_OFF.java
@@ -1,6 +1,7 @@
 package main.java.it.unipv.posfw.smartdab.core.domain.model.dispositivo.attuatori.lampadaON_OFF;
 
 import main.java.it.unipv.posfw.smartdab.adapter.facade.AttuatoreFacade;
+import main.java.it.unipv.posfw.smartdab.core.domain.enums.DispositivoParameters;
 import main.java.it.unipv.posfw.smartdab.core.domain.model.parametro.ObservableParameter;
 
 public class Lampada_ON_OFF extends AttuatoreFacade {
@@ -11,6 +12,7 @@ public class Lampada_ON_OFF extends AttuatoreFacade {
 	
 	public Lampada_ON_OFF(String id, Lampada_Communicator c, ObservableParameter parameter, int intensita) {
 		super(id, c, parameter);
+		super.getTopic().setParameter(DispositivoParameters.LUMINOSITA);
 		this.intensita = intensita;
 		illuminazione = 0;
 		c.setDevicePort(this);
