706866812d020a3d689762e4627c9723f2c5cfb0
AlezGradient <AlessandroGere.ingenito01@universitadipavia.it>
2026-01-22 12:46:33 +0100

    Interfacce DAO e impl

--- src/main/java/it/unipv/posfw/smartdab/infrastructure/persistence/mysql/dao/CommunicationDAO.java
+++ src/main/java/it/unipv/posfw/smartdab/infrastructure/persistence/mysql/dao/CommunicationDAO.java
@@ -1,55 +1,9 @@
 package it.unipv.posfw.smartdab.infrastructure.persistence.mysql.DAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import it.unipv.posfw.smartdab.core.beans.CommunicationPOJO;
 
-public class CommunicationDAO {
-	private String schema;
-	private Connection connection;
-	public final int MAX_N = 100;
-	
-	public CommunicationDAO() {
-		this.schema = "";
-	}
-	
-	public ArrayList<CommunicationPOJO> selectN(int n) {
-		ArrayList<CommunicationPOJO> result = new ArrayList<>();
-		
-		if(n > MAX_N) {
-			System.out.println("Il numero di comunicazioni da selezionare è troppo elevato");
-			return result;
-		}
-		
-		// connection = DBConnection.startConnection(connection, schema);
-		PreparedStatement s;
-		ResultSet r;
-		
-		try {
-			String query = "SELECT * from Dispositivi LIMIT " + n;
-			s = connection.prepareStatement(query);
-			r = s.executeQuery(query);
-			
-			while(r.next()) {
-				CommunicationPOJO c = new CommunicationPOJO(
-														r.getString(1),
-														r.getString(2),
-														r.getString(3),
-														r.getObject(4),
-														r.getString(5),
-														r.getTimestamp(6).toLocalDateTime()
-														);
-				result.add(c);
-			}
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		// DBConnection.closeConnection(conn);
-		return result;
-	}
+public interface CommunicationDAO {
+	public ArrayList<CommunicationPOJO> selectN(int n);
 }
--- src/main/java/it/unipv/posfw/smartdab/infrastructure/persistence/mysql/dao/DispositivoDAO.java
+++ src/main/java/it/unipv/posfw/smartdab/infrastructure/persistence/mysql/dao/DispositivoDAO.java
@@ -1,58 +1,9 @@
 package it.unipv.posfw.smartdab.infrastructure.persistence.mysql.DAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import it.unipv.posfw.smartdab.core.beans.DispositivoPOJO;
-import it.unipv.posfw.smartdab.core.domain.enums.DispositivoParameter;
-import it.unipv.posfw.smartdab.core.domain.enums.DispositivoStates;
 
-public class DispositivoDAO {
-	private String schema;
-	private Connection connection;
-	public final int MAX_N = 100;
-	
-	public DispositivoDAO() {
-		this.schema = "";
-	}
-	
-	public ArrayList<DispositivoPOJO> selectN(int n) {
-		ArrayList<DispositivoPOJO> result = new ArrayList<>();
-		
-		if(n > MAX_N) {
-			System.out.println("Il numero di dispositivi da selezionare è troppo elevato");
-			return result;
-		}
-		
-		// connection = DBConnection.startConnection(connection, schema);
-		PreparedStatement s;
-		ResultSet r;
-		
-		try {
-			String query = "SELECT * from Dispositivi LIMIT " + n;
-			s = connection.prepareStatement(query);
-			r = s.executeQuery(query);
-			
-			while(r.next()) {
-				DispositivoPOJO d = new DispositivoPOJO(r.getString(1), 
-														r.getString(2), 
-														DispositivoParameter.valueOf(r.getString(3)), 
-														r.getString(4), 
-														DispositivoStates.valueOf(r.getString(5)), 
-														r.getBoolean(6),
-														r.getTimestamp(7).toLocalDateTime(), 
-														r.getString(8)
-														);
-				result.add(d);
-			}
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		// DBConnection.closeConnection(conn);
-		return result;
-	}
+public interface DispositivoDAO {
+	public ArrayList<DispositivoPOJO> selectN(int n);
 }
