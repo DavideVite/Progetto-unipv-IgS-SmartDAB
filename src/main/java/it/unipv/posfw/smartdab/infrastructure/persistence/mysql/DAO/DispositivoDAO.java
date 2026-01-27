c312697fee5e198dc3a73a9cb22060ed452c07a3
DavideVite <davide.vitello01@universitadipavia.it>
2026-01-27 14:07:03 +0100

    Delete src/main/java/it/unipv/posfw/smartdab/infrastructure/persistence/mysql/DAO directory

--- src/main/java/it/unipv/posfw/smartdab/infrastructure/persistence/mysql/DAO/DispositivoDAO.java
+++
@@ -1,9 +0,0 @@
-package it.unipv.posfw.smartdab.infrastructure.persistence.mysql.DAO;
-
-import java.util.ArrayList;
-
-import it.unipv.posfw.smartdab.core.beans.DispositivoPOJO;
-
-public interface DispositivoDAO {
-	public ArrayList<DispositivoPOJO> selectN(int n);
-}
