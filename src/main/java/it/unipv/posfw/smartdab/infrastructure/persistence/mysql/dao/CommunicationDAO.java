bf07ea65fce657ebfe8f5b95578d217a80186042
DavideVite <davide.vitello01@universitadipavia.it>
2026-01-27 14:46:08 +0100

    risoluzioni conflitto DAO-dao

--- src/main/java/it/unipv/posfw/smartdab/infrastructure/persistence/mysql/dao/CommunicationDAO.java
+++
@@ -1,9 +0,0 @@
-package it.unipv.posfw.smartdab.infrastructure.persistence.mysql.dao;
-
-import java.util.ArrayList;
-
-import it.unipv.posfw.smartdab.core.beans.CommunicationPOJO;
-
-public interface CommunicationDAO {
-	public ArrayList<CommunicationPOJO> selectN(int n);
-}
