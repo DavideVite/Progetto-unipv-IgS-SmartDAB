c63e2b5606a68bb66ee95585a88fa6f160464b42
BeatriceBertone <beatrice.bertone01@universitadipavia.it>
2026-02-01 17:47:44 +0100

    Aggiunto salvataggio PIN dopo il primo inserimento

--- src/main/java/it/unipv/posfw/smartdab/ui/SmartDABApp.java
+++ src/main/java/it/unipv/posfw/smartdab/ui/SmartDABApp.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import it.unipv.posfw.smartdab.core.service.PersistenzaManager;
 import it.unipv.posfw.smartdab.ui.controller.AutenticazioneController;
 import it.unipv.posfw.smartdab.ui.controller.MainController;
 import it.unipv.posfw.smartdab.ui.view.autenticazione.ConfigurazionePinPanel;
@@ -12,7 +13,19 @@ import it.unipv.posfw.smartdab.ui.view.autenticazione.LoginProduttorePanel;
 
 public class SmartDABApp {
    public static void main(String[] args) {
-       SwingUtilities.invokeLater(() -> {
+    	  
+    	// prova per eliminare il pin e far partire tutto dall'inserimento password
+    	   /* try {
+    	        java.util.prefs.Preferences.userNodeForPackage(PersistenzaManager.class).clear();
+    	        System.out.println("PIN eliminato");
+    	    } catch (Exception e) {
+    	        e.printStackTrace();
+    	    } */
+    	   
+    	   SwingUtilities.invokeLater(() -> {
+        	
+        	
+        	String pinSalvato = PersistenzaManager.caricaPin();
 
        	//finestra di login
        	JFrame autFrame = new JFrame("Accesso");
@@ -27,6 +40,17 @@ public class SmartDABApp {
            //aggiunge i pannelli assegnando un nome
            container.add(new LoginProduttorePanel(autController), "LOGIN");
            container.add(new ConfigurazionePinPanel(autController), "PIN_SETUP");
+           
+           if (pinSalvato == null) {
+        	   //primo avvio: non c'è pin, mostriamo password produttore
+        	   cl.show(container, "LOGIN");
+        	   System.out.println("Primo avvio: richiesta password produttore.");
+           } else {
+               // AVVII SUCCESSIVI: Il PIN esiste, saltiamo la password e andiamo al PIN
+               cl.show(container, "PIN_SETUP");
+               System.out.println("PIN trovato: salto alla schermata di sblocco.");	   
+           }
+           
            //assemblaggio del frame
            autFrame.add(container);
            autFrame.pack();
