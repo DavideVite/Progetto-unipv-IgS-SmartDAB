e353fbfd554d019d05e61a1246a4021c81974bf0
BeatriceBertone <beatrice.bertone01@universitadipavia.it>
2026-02-01 14:28:28 +0100

    Feature/gestioneGuasti (#79)
    * Aggiunta gestione malfunzionamenti
    * Aggiunto salvataggio PIN dopo il primo inserimento

--- src/main/java/it/unipv/posfw/smartdab/ui/view/autenticazione/ConfigurazionePinPanel.java
+++ src/main/java/it/unipv/posfw/smartdab/ui/view/autenticazione/ConfigurazionePinPanel.java
@@ -33,14 +33,22 @@ public class ConfigurazionePinPanel extends JPanel{
 		gbc.gridy = 2;
 		add(txtPin, gbc);
 		
-		btnConferma = new JButton("Configura PIN e accedi");
+		btnConferma = new JButton("Accedi");
 		btnConferma.setPreferredSize(new Dimension(200, 40));
 		gbc.gridy = 3;
 		add(btnConferma, gbc);
 		
 		btnConferma.addActionListener(e -> {
 			String pinInserito = txtPin.getText();
-			controller.configuraNuovoPin(pinInserito);
+			if (PersistenzaManager.caricaPin() == null) {
+		        //primo avvio -> salviamo il PIN scelto
+		        System.out.println("Salvataggio nuovo PIN...");
+		        controller.configuraNuovoPin(pinInserito);
+		    } else {
+		        //avvii successivi -> verifichiamo se è quello giusto
+		        System.out.println("Verifica PIN esistente...");
+		        controller.verificaPinEsistente(pinInserito);
+		    }
 		});
 	}
 	public void svuotaCampo() {
